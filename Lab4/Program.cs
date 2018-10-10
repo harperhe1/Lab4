b22a7066a645cefbdd4fdaca67cce182ba5113ed
Heather <harperhe@hotmail.com>
2018-10-10 16:45:30 -0400

    initial commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Learn your squares and cubes!");
+                Console.WriteLine("Enter an integer:");
+
+                int num = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine("Number    " + "Squared   " + "Cubed");
+                Console.WriteLine("======    " + "======    " + "======    ");
+
+                for (int i = 1; i <= num; i++)
+                {
+                    int square = i * i;
+                    int cube = i * i * i;
+                    Console.WriteLine(i + ("        ") + square + ("       ") + cube);
+                }
+                
+
+                Console.WriteLine("Continue?(y/n)");
+            } while (Console.ReadLine() == "y");
+        }
+    }
+}
