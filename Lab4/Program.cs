3275045a56e9fa5a484ecc004989dbc7a4db6009
Heather <harperhe@hotmail.com>
2018-10-15 14:34:19 -0400

    added while

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -10,13 +10,15 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            do
+            string doAgain;
+            bool cont = true;
+            while (cont)
             {
                 Console.WriteLine("Learn your squares and cubes!");
                 Console.WriteLine("Enter an integer:");
 
                 int num = int.Parse(Console.ReadLine());
-                
+
                 Console.WriteLine("Number    " + "Squared   " + "Cubed");
                 Console.WriteLine("======    " + "======    " + "======    ");
 
@@ -26,10 +28,33 @@ namespace Lab4
                     int cube = i * i * i;
                     Console.WriteLine(i + ("        ") + square + ("       ") + cube);
                 }
-                
 
-                Console.WriteLine("Continue?(y/n)");
-            } while (Console.ReadLine() == "y");
+                bool cont2 = true;
+                while (cont2)
+                {
+                    Console.WriteLine("Continue?(y/n)");
+                    doAgain = Console.ReadLine().ToLower();
+                    if (doAgain == "y")
+                    {
+                        cont2 = false;
+                    }
+                    else if (doAgain == "n")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        cont2 = false;
+                        cont = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input. Try Again!");
+                    }
+
+
+
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
+
